572f6dd4d3ff26d1425adbe1cb37c76b4d4abf4c
Osorio3408 <yuliamwow@gmail.com>
2026-03-13 10:47:52 -0500

    fix: limpiar dependencias para deploy

--- matriz_riesgo/settings.py
+++ matriz_riesgo/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
